96ff9cdaa36cfff831dd5c6cbb82b61e43dba2b7
bozhou-hash <bz04cheng@gmail.com>
2026-03-24 16:28:00 +0000

    Edited main.py to include different game modes

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from env.connect4_env import Connect4Env
 from ui.game_ui import GameUI
 from agents.random_agent import RandomAgent
-from config import PLAYER_TWO
+from agents.minimax_agent import MinimaxAgent
+from config import PLAYER_ONE, PLAYER_TWO
 
 
 def main():
     env = Connect4Env()
     ui = GameUI(env)
 
-    random_agent = RandomAgent(player=PLAYER_TWO)
-    ui.run_human_vs_agent(agent=random_agent)
+    # Run UI for human vs human
+    # ui.run_human_vs_human()
+
+    # Run UI for human vs random agent
+    # random_agent = RandomAgent(player=PLAYER_TWO)
+    # ui.run_human_vs_agent(agent=random_agent)
+
+    # Run UI for human vs minimax agent
+    minimax_agent = MinimaxAgent(player=PLAYER_TWO, depth=4)
+    ui.run_human_vs_agent(minimax_agent, human_player=PLAYER_ONE)
 
 
 if __name__ == "__main__":
